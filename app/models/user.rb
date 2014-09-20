bf7f637204fdb5723ee6ba72b96e8c43450b06e9
Randall Leung <leungran@gmail.com>
2014-09-20 15:25:25 -0700

    add pw reset and authenticate in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
 	
 	has_many :completes
 	has_many :codes, :through => :completes
+
+
+	def set_password_reset
+		self.code = SecureRandom.urlsafe_base64
+		self.expires_at = 4.hours.from_now
+		self.save!
+	end
+
+	def self.authenticate username, password
+		User.find_by_username(username).try(:authenticate,password)
+	end
+
 end
